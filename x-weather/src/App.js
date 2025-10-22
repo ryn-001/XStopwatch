9a0ba7fb306694cec37128c58ad8e39d407ccf19
Aryan Khaire <aryan.khaire001@gmail.com>
2025-10-22 23:32:38 +0530

    Minor test bugs

--- x-weather/src/App.js
+++ x-weather/src/App.js
@@ -48,24 +48,24 @@ function App() {
 			</div>			
 
 			
-				{loading ? (<div className="results">Loading data...</div>) : (
-					<div className='results'>
-						<div id="tempreature">
+				{loading ? (<p>Loading data...</p>) : (
+					<div className='weather-cards'>
+						<div className="weather-card" id="tempreature">
 							<h3>Tempreature</h3>
 							<p>{ !data.current.temp_c ? ('-') : (`${data.current.temp_c}°C`)}</p>
 						</div>
 
-						<div id="humidity">
+						<div id="humidity" className="weather-card">
 							<h3>Humidity</h3>
 							<p>{ !data.current.humidity ? ('-') : (`${data.current.humidity}%`)}</p>
 						</div>
 
-						<div id="condition">
+						<div id="condition" className="weather-card">
 							<h3>Condition</h3>
 							<p>{ !data.current.condition.text ? ('-') : (`${data.current.condition.text}`)}</p>
 						</div>
 
-						<div id="wind-speed">
+						<div id="wind-speed" className="weather-card">
 							<h3>Wind Speed</h3>
 							<p>{ !data.current.wind_kph ? ('-') : (`${data.current.wind_kph} kph`)}</p>
 						</div>
