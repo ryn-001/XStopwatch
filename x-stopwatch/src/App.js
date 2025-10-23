5793150121a790c07c4aa1a2ddd018ba37e379b8
Aryan Khaire <aryan.khaire001@gmail.com>
2025-10-23 21:11:07 +0530

    done

--- x-stopwatch/src/App.js
+++ x-stopwatch/src/App.js
@@ -1,39 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 import { useState, useRef } from 'react';
 
 function App() {
   const [seconds,setSeconds] = useState(0);
-  const [minutes,setMinutes] = useState(0);
   const [button,setButton] =  useState('Start');
-  const time = useRef();
+  const time = useRef(0);
+  const intervalTime = useRef();
 
   const handleStartStop = async (e) => {
     if(e.target.value === 'Start'){
       setButton('Stop');
-      time.current = setInterval(() => {
-        if(seconds + 1 === 60){
-          setMinutes(prev => prev+1);
-          setSeconds(0);
-        }
-        else setSeconds(prev => prev+1);
-      }, 1000);
+      intervalTime.current = setInterval(() => {
+        time.current += 1;
+        setSeconds(time.current);
+      }, 1000)
     }else{
       setButton('Start');
-      clearInterval(time);
-      setMinutes(0), seconds(0);
+      clearInterval(intervalTime.current);
     }
   }
 
   const handleReset = () => {
-    clearInterval(time);
-    setSeconds(0), setMinutes(0);
+    clearInterval(intervalTime.current);
+    time.current = 0;
+    setSeconds(0);
   }
+
+  const minutes = Math.floor(seconds/60);
 
   return (
     <div className="App">
       <h1>Stopwatch</h1>
-      <p>Time: {minutes > 10 ? minutes : `0${minutes}`}:{seconds > 10 ? seconds : `0${seconds}`}</p>
+      <p>Time: {minutes >= 10 ? minutes : `0${minutes}`}:{seconds%60 >= 10 ? seconds%60 : `0${seconds%60}`}</p>
       <button value={button} onClick={handleStartStop}>{button}</button>
       <button onClick={handleReset}>Reset</button>
     </div>
